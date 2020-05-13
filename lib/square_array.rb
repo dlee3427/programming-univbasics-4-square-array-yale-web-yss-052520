718369b8c2d53c4f4a1d91b087df725c3fa63b63
David Lee <david.lee.sl2634@yale.edu>
2020-05-13 16:09:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0 
   new_array = []
   while array[counter] do 
-    new_array.push(array[counter].to_s ** 2)
+    new_array.push(array[counter].to_i ** 2)
     counter += 1 
   end
   new_array
